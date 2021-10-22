b60c1d694c411f79e37fce31fb47ed3fafb7b9d4
Ilham Yusuf Balanda <43397437+silentghost13@users.noreply.github.com>
2021-10-22 16:07:50 +0700

    Update weather_app.py

--- Weather_App/weather_app.py
+++ Weather_App/weather_app.py
@@ -1,12 +1,33 @@
-import requests
-from pprint import pprint
-#https://www.youtube.com/watch?v=SqvVm3QiQVk&t=40s
-API_key = 'b979cba50050174b8c3044fcaaf5eced'
+#https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
+import requests 
+import json
 
-city_name = input('Enter a city : ')
+api_key = '271bc5eb995eab041bc0919bae99bc6c'
+base_url = 'https://api.openweathermap.org/data/2.5/weather?q='
+city_name = input('Enter city name : ')
 
-base_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' + API_key
+complete_url = base_url + city_name + '&units=metric&appid=' + api_key 
 
-weather_data = requests.get(base_url).json()
+response = requests.get(complete_url)
 
-pprint(weather_data)
+data = response.json()
+
+if data['cod'] != '404':
+	main = data['main']
+	current_temperature = main['temp']
+	current_pressure = main['pressure']
+	current_humidity = main['humidity']
+	z = data['weather']
+	weather_description = z[0]['description']
+
+	print(' Temperature (in celcius unit) = ' +
+					str(current_temperature) +
+		'\n atmospheric pressure (in hPa unit) = ' +
+					str(current_pressure) +
+		'\n humidity (in percentage) = ' +
+					str(current_humidity) +
+		'\n description = ' +
+					str(weather_description))
+
+else:
+	print(' City Not Found ')
